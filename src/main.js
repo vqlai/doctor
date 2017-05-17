4739a6a3b71b51e03813f7f936d7ef97c3a1d355
vqlai <695850246@qq.com>
2017-05-17 20:55:31 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,23 @@
+﻿import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routes from './routers';
+import FastClick from 'fastclick';
+import common from './libs/common.js';
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+Vue.use(VueRouter);
+const router = new VueRouter({routes});
+
+// FastClick挂载在body上，改造所有元素的click事件
+FastClick.attach(document.body);
+// 给Vue的原型上添加common,可以在Vue的实例中随意调用它
+Vue.prototype.common = common;
+new Vue({router}).$mount('#app');
+if (!localStorage.token) {
+	common.doAjax("Token/noAuthCreate",{isNeedToken:false})
+	.done(result=>{
+		if(result.code == 1){
+			localStorage.token = result.extra.sess.token;
+		}
+	})
+}
---
+++ src/routers.js
@@ -0,0 +1,120 @@
+﻿const login = resolve => {
+     require(['./views/user/login.vue'], resolve);
+}
+const routers = [{
+    path: '/',
+    name: 'base',
+    component:login
+}, {
+    path: '/user/login',
+    name: 'login',
+    component:login
+}, {
+    path: '/user/forget',
+    name: 'forget',
+    component(resolve) {
+        require(['./views/user/forget.vue'], resolve);
+    }
+}, {
+    path: '/user/register',
+    name: 'register',
+    component(resolve) {
+       require(['./views/user/register.vue'], resolve);
+    }
+}, {
+    path: '/user/protocol',
+    name: 'protocol',
+    component(resolve) {
+        require(['./views/user/protocol.vue'], resolve);
+    }
+}, {
+    path: '/user/reset',
+    name: 'reset',
+    component(resolve) {
+        require(['./views/user/reset.vue'], resolve);
+    }
+},{
+    path: '/doctor/index',
+    name: 'index',
+    component(resolve) {
+        require(['./views/doctor/index.vue'], resolve);
+    }
+},{
+    path: '/doctor/doctor',
+    name: 'doctor',
+    component(resolve) {
+        require(['./views/doctor/doctor.vue'], resolve);
+    }
+},{
+    path: '/doctor/search',
+    name: 'search',
+    component(resolve) {
+        require(['./views/doctor/search.vue'], resolve);
+    }
+},{
+    path: '/doctor/evaluation',
+    name: 'evaluation',
+    component(resolve) {
+        require(['./views/doctor/evaluation.vue'], resolve);
+    }
+},{
+    path: '/doctor/advice',
+    name: 'advice',
+    component(resolve) {
+        require(['./views/doctor/advice.vue'], resolve);
+    }
+},{
+    path: '/doctor/homedr',
+    name: 'homedr',
+    component(resolve) {
+        require(['./views/doctor/home_doctor.vue'], resolve);
+    }
+},{
+    path: '/doctor/imagead',
+    name: 'imagead',
+    component(resolve) {
+        require(['./views/doctor/img_advice.vue'], resolve);
+    }
+},{
+    path: '/doctor/record',
+    name: 'record',
+    component(resolve) {
+        require(['./views/doctor/buy_record.vue'], resolve);
+    }
+},{
+    path: '/doctor/center',
+    name: 'center',
+    component(resolve) {
+        require(['./views/doctor/center.vue'], resolve);
+    }
+},{
+    path: '/doctor/pay',
+    name: 'pay',
+    component(resolve) {
+        require(['./views/doctor/pay.vue'], resolve);
+    }
+},{
+    path: '/chat/chat',
+    name: 'chat',
+    component(resolve) {
+        require(['./views/chat/chat.vue'], resolve);
+    }
+},{
+    path: '/help/detail',
+    name: 'detail',
+    component(resolve) {
+        require(['./views/help/detail.vue'], resolve);
+    }
+},{
+    path: '/help/list',
+    name: 'list',
+    component(resolve) {
+        // require.ensure(['./views/doctor/doctor.vue'], () => {
+        //     resolve(require('./views/doctor/doctor.vue'));
+        // });
+        require(['./views/help/list.vue'], resolve);
+    }
+}
+];
+
+export default routers;
